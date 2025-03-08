aca3fe3bb84dce8b66bbe3833d94ddd50305d8f7
Ichika <131859874+ichika06@users.noreply.github.com>
2025-03-08 16:36:56 +0800

    Add files via upload

---
+++ proxy.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const httpProxy = require("http-proxy");
+
+const app = express();
+const proxy = httpProxy.createProxyServer({});
+const ESP_STREAM_URL = "http://192.168.1.100:81/stream";
+
+app.get("/stream", (req, res) => {
+  proxy.web(req, res, { target: ESP_STREAM_URL, changeOrigin: true });
+});
+
+app.listen(3000, () => console.log("Proxy running on port 3000"));
